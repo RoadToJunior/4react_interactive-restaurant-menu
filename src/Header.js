7c5ef4a8e67ac93a151425a66f9c850b34ec4a19
NowWeAre-Free <kontodarm@wp.pl>
2023-06-09 10:14:52 +0200

    add content to component, filter method used propsed list

--- src/Header.js
+++ src/Header.js
@@ -1,3 +1,14 @@
 const Header = (props) => {
-  return <header>Podsumowanie zamówienia</header>;
+  const activeItems = props.items.filter((item) => item.active === true);
+  const number = activeItems.length;
+  return (
+    <header>
+      <h2>Wielkość zamówienia: {number}</h2>
+      <h2>
+        Do zapłaty:{" "}
+        {number ? `${number * 10} złotych` : "nie kupujesz, to nie płacisz"}
+        złotych
+      </h2>
+    </header>
+  );
 };
